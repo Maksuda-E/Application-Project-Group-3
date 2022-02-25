3581807f4cf8d3b41c0f272e903656885ef18c1c
laforetd-alice64 <98922987+laforetd-alice64@users.noreply.github.com>
2022-02-26 01:33:12 +0800

    i completed report page which includes found list and missing list without changing your original style

--- navigation/screens/ReportsScreen/index.js
+++ navigation/screens/ReportsScreen/index.js
@@ -1,18 +1,87 @@
-import React from 'react';
-import { 
-  View, 
-  Text,
-  ImageBackground,
+import React, { useState, useEffect } from 'react';
+import {
+    View,
+    Text,
+    ImageBackground, ScrollView,
 } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import styles from './styles';
+import ReportItem from "../../../components/ReportItem";
+import {firestore} from "../../../firebaseConfig";
 
 const ReportsScreen = ({navigation}) => {
+    let [missingpets, setMissingpets] = useState([]);
+    let [foundpets, setFoundpets] = useState([]);
+    console.log("111")
+
+    useEffect(() => {
+        console.log("222")
+        const subscriber = async () => {
+            try {
+                console.log("333")
+                let result = [];
+                let data = await firestore.collection('missingPets').get();
+
+                data.forEach(e => {
+
+                    if (e.id) {
+                        result.push({
+                            id: e.id,
+                            type: e.data().type,
+                            breed: e.data().breed,
+                            description: e.data().description,
+                        });
+                    }
+                })
+                console.log(result)
+
+                setMissingpets(result);
+                result = [];
+
+                data = await firestore.collection('foundPets').get();
+
+                data.forEach(e => {
+
+                    if (e.id) {
+                        result.push({
+                            id: e.id,
+                            type: e.data().type,
+                            breed: e.data().breed,
+                            description: e.data().description,
+                        });
+                    }
+                })
+
+                setFoundpets(result);
+                console.log(result)
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        subscriber();
+    }, []);
   return (
     <View style={styles.container}>
       <ImageBackground source={ require('../../../assets/data/images/background.jpg') } style={styles.background} >
         <Text style={styles.title}>Report</Text>
+
+          <Text style={styles.subTitle}>Missing Pets List</Text>
+          <ScrollView>
+              {
+                  missingpets.map((pet) =>
+                      <ReportItem pet={pet} />)
+              }
+          </ScrollView>
+
+          <Text style={styles.subTitle}>Found Pets List</Text>
+          <ScrollView>
+              {
+                  foundpets.map((pet) =>
+                      <ReportItem pet={pet} />)
+              }
+          </ScrollView>
+
       </ImageBackground>
     </View>
   )
